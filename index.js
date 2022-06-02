578e82f936320a7dada1acb9f9b6f6234f90afec
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-02 10:38:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const app = express()
+const http = require('http');
+const expressServer = http.createServer(app)
+const port = 5000 || process.env.PORT;
+
+const {Server} = require("socket.io")
+const io = new Server(expressServer)
+
+io.on('connection', (socket) => {
+    console.log("New User Connected");
+
+    socket.on('disconnect', () => {
+        console.log("User Disconnected");
+    })
+
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+expressServer.listen(port, () => {
+    console.log("Server Run At 5000");
+})
